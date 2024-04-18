6d217afa5260a81ff56f8fc48dfbc1b7b10cd224
SantinoSchiavoni <2319332@ucc.edu.ar>
2024-04-18 13:35:02 -0300

    Add card

---
+++ Proyecto/add-card.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Selecciona el botón para agregar tarjeta
+    var addButton = document.getElementById('add-card');
+
+    // Selecciona el botón de bloqueo
+    var lockButton = document.querySelector('.fa-lock');
+
+    // Selecciona la div de la tarjeta
+    var cardDiv = document.querySelector('.card');
+
+    // Carga los datos de la tarjeta del almacenamiento local si existen
+    var cardData = localStorage.getItem('cardData');
+    if (cardData) {
+        cardDiv.innerHTML = cardData;
+    }
+
+    // Agrega un evento de clic al botón para agregar tarjeta
+    addButton.addEventListener('click', function() {
+        // Cambia el estilo de la div de la tarjeta para que sea visible
+        cardDiv.style.display = 'flex';
+
+        // Hace que el botón para agregar tarjeta desaparezca
+        addButton.style.display = 'none';
+    });
+
+    // Agrega un evento de clic al botón de bloqueo
+    lockButton.addEventListener('click', function() {
+        // Guarda los datos de la tarjeta en el almacenamiento local
+        localStorage.setItem('cardData', cardDiv.innerHTML);
+
+        // Cambia el estilo de la div de la tarjeta para que desaparezca
+        cardDiv.style.display = 'none';
+
+        // Hace que el botón para agregar tarjeta aparezca
+        addButton.style.display = '';
+    });
+});
